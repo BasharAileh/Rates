49b6ee4d78f05279f4f3b2fb6e8c8bd1b75b6e09
bara-shaban <barashaban03@gmail.com>
2024-11-24 02:01:08 +0300

    updates on ios folder and renaming the main app folder cause it causes errors somettimes due to the naming convintion

---
+++ gp_app/lib/init_page.dart
@@ -0,0 +1,49 @@
+import 'dart:developer' as devtools show log;
+
+import 'package:flutter/material.dart';
+import 'package:rates/Pages/registration/login_page.dart';
+import 'package:rates/constants/aspect_ratio.dart';
+import 'package:rates/services/auth/auth_service.dart';
+
+final Future<void> _initializeFirebase =
+    Future.delayed(const Duration(seconds: 2), () async {
+  await AuthService.firebase().initialize();
+});
+
+class InitPage extends StatelessWidget {
+  const InitPage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    AspectRatios.init(
+        MediaQuery.of(context).size.height, MediaQuery.of(context).size.width);
+    return FutureBuilder(
+      future: _initializeFirebase,
+      builder: (context, snapshot) {
+        switch (snapshot.connectionState) {
+          case ConnectionState.waiting:
+            return const CircularProgressIndicator();
+          case ConnectionState.done:
+            final user = AuthService.firebase().currentUser;
+            devtools.log('User: $user');
+            if (user != null) {
+              if (user.isAnonymous) {
+                return const LoginPage();
+              }
+              if (user.isEmailVerified) {
+                return const LoginPage();
+              } else {
+                return const LoginPage(); //this is the one you change whenever you want to run your page
+                //don't text me 'what line to change, so i can run my page please :)'
+              }
+            } else {
+              return const LoginPage();
+            }
+
+          default:
+            devtools.log('2');
+            return const CircularProgressIndicator();
+        }
+      },
+    );
+  }
+}
